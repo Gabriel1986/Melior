fbb722aafdc38b706c896575f0f990102d24882e
Mark Bracke <mark@tactics.be>
2020-05-03 12:41:05 +0200

    Made content root point to the correct folder

--- src/Server/Program.fs
+++ src/Server/Program.fs
@@ -1,17 +1,43 @@
 namespace Server
 
+open System.IO
 open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.Hosting
+open Serilog
+open Serilog.Events
+open Serilog.Sinks.SystemConsole.Themes
 
 module Program =
     let exitCode = 0
 
     let CreateHostBuilder args =
+        let contentPath =
+            Directory.GetCurrentDirectory()
+            |> sprintf "%s/../Client/public"
+            |> Path.GetFullPath
+
         Host.CreateDefaultBuilder(args)
+            .UseContentRoot(contentPath)
             .ConfigureWebHostDefaults(fun webBuilder ->
-                webBuilder.ConfigureKestrel(fun options ->
-                    options.ListenLocalhost(8091)
-                ).UseStartup<Startup>() |> ignore
+                webBuilder
+                    .ConfigureKestrel(fun options ->  options.ListenLocalhost(8091))
+                    .UseStartup<Startup>()
+                    .UseWebRoot(contentPath)
+                |> ignore
+                    //TODO: read config from configuration file.
+                    //.UseSerilog(configureLogger = (fun (context: WebHostBuilderContext)  (configuration: LoggerConfiguration) ->
+                    //    configuration
+                    //        .MinimumLevel.Debug()
+                    //        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                    //        .MinimumLevel.Override("System", LogEventLevel.Warning)
+                    //        .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+                    //        .Enrich.FromLogContext()
+                    //        .WriteTo.File(@"server_log.txt")
+                    //        .WriteTo.Console(
+                    //            outputTemplate = "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}",
+                    //            theme = AnsiConsoleTheme.Literate
+                    //        )
+                    //    |> ignore
             )
 
     [<EntryPoint>]
